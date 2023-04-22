942c56200571889942538ff0b05bc6afc2929107
2nav <n-raj@outlook.com>
2023-04-23 00:49:03 +0530

    #todo fix everything

--- ilp.py
+++ ilp.py
@@ -5,20 +5,42 @@ import math
 import numpy as np
 
 def gomory(filename):   
-    A,b,c = takeinput
+    #A,b,c = takeinput
+
+    #Check Ax = b
+    A = np.array([[5,7,4,3,1,0], [7,0,2,1,0,1]])
+    b = np.array([14,10])
+    c = np.array([-8,-11,-6,-4,0,0])
     
     x_opt,table,flag,basis_list = tableau(A,b,c)  
     feasible = True
 
     if(flag == 1):
         return None
+    
+    n = len(c)
 
-    while(feasible and not(isInt(x_opt))):
+    while(feasible):
+        flag = True
+        for i in range(len(x_opt)):
+            x = x_opt[i]
+            if(frac(x)>=1e-6 and frac(x)<=1-1e-6):
+                flag = False
+            else:
+                x_opt[i] = round(x_opt[i])
+        if(flag == True):
+            break
+
+
         i = choice(x_opt)
-        table = addcondition(table,i)
-        x_opt, flag = dualsimplex(table,len(c),basis_list)
+        table,basis_list = addcondition(table,i,basis_list)
+        n += 1
+        x_opt, flag = dualsimplex(table,n,basis_list)
         if(flag == 1):
             feasible = False
+            break
+        
+        print(x_opt)
 
     if(feasible == True):
         return x_opt #TODO verify data type - # an array of n integers
@@ -28,3 +50,5 @@ def gomory(filename):
 '''
 Check - Potential cycling? some preference
 '''
+
+print(gomory("Yo"))
