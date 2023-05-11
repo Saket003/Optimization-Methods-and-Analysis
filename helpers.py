9e9b3ed284cc7cb9a468ff5862f34c37d16f1132
2nav <n-raj@outlook.com>
2023-05-11 14:29:54 +0530

    autopep8

--- helpers.py
+++ helpers.py
@@ -2,54 +2,59 @@ import numpy as np
 import math
 from fractions import Fraction
 
+
 def takeinput(filename):
     with open(filename) as f:
         line = f.readlines()
-        n,m = (line[0].strip()).split()
+        n, m = (line[0].strip()).split()
         n = int(n)
         m = int(m)
 
         b = (line[1].strip()).split()
-        b = np.asarray(b,dtype=int) + Fraction()
+        b = np.asarray(b, dtype=int) + Fraction()
 
         c = (line[2].strip()).split()
-        c = np.asarray(c,dtype=int) + Fraction()
+        c = np.asarray(c, dtype=int) + Fraction()
         c = -1*c
 
-        A = np.zeros((m,n), dtype=int) + Fraction()
+        A = np.zeros((m, n), dtype=int) + Fraction()
         for i in range(m):
             temp = (line[3+i].strip()).split()
-            A[i,:] = np.asarray(temp,dtype=int) + Fraction()
-    return A,b,c,n,m
+            A[i, :] = np.asarray(temp, dtype=int) + Fraction()
+    return A, b, c, n, m
 
-def convert(A,new_n,c):
-    m,n = A.shape
-    A_new = np.zeros((m,new_n), dtype=int) + Fraction()
-    A_new[0:m,0:n] = A
-    A_new[0:m,n:new_n] = np.eye(m,dtype=int) + Fraction()
+
+def convert(A, new_n, c):
+    m, n = A.shape
+    A_new = np.zeros((m, new_n), dtype=int) + Fraction()
+    A_new[0:m, 0:n] = A
+    A_new[0:m, n:new_n] = np.eye(m, dtype=int) + Fraction()
     c_new = np.zeros(new_n, dtype=int) + Fraction()
     c_new[0:len(c)] = c
-    return A_new,c_new
+    return A_new, c_new
 
-def choice(x):  
+
+def choice(x):
     max_frac = 0
     max_index = 0
     for i in range(len(x)):
-        if(frac(x[i])>max_frac):
-            max_frac,max_index = frac(x[i]),i+1
+        if (frac(x[i]) > max_frac):
+            max_frac, max_index = frac(x[i]), i+1
     return max_index
 
+
 def addcondition(table, index, basis_list):
-    a,b = table.shape
-    new_table = np.zeros((a+1,b+1), dtype=int) + Fraction()
-    new_table[0:a,0:b] = table
-    new_table[a,b] = 1
+    a, b = table.shape
+    new_table = np.zeros((a+1, b+1), dtype=int) + Fraction()
+    new_table[0:a, 0:b] = table
+    new_table[a, b] = 1
     basis_list = np.append(basis_list, b)
 
     row_no = np.where(basis_list == index)[0][0] + 1
     for j in range(b):
-        new_table[a,j] = -1*frac(table[row_no,j])
-    return new_table,basis_list
+        new_table[a, j] = -1*frac(table[row_no, j])
+    return new_table, basis_list
+
 
 def frac(x):
     return Fraction(x) - int(math.floor(x))
--- ilp.py
+++ ilp.py
@@ -4,22 +4,23 @@ import math
 import numpy as np
 from fractions import Fraction
 
-def gomory(filename):   
-    A,b,c,initial_n,initial_m = takeinput(filename)
-    n = initial_n + initial_m
-    A,c = convert(A,n,c)
 
-    #TODO Verify #Check initial table wrongish?
-    x_opt,table,flag,basis_list = tableau(A,b,c)
-    x_opt, flag = simplex_mod(table,n,basis_list)
-    if(flag == 1):
+def gomory(filename):
+    A, b, c, initial_n, initial_m = takeinput(filename)
+    n = initial_n + initial_m
+    A, c = convert(A, n, c)
+
+    # TODO Verify #Check initial table wrongish?
+    x_opt, table, flag, basis_list = tableau(A, b, c)
+    x_opt, flag = simplex_mod(table, n, basis_list)
+    if (flag == 1):
         return None
-    
-    a,b = table.shape
-    
+
+    a, b = table.shape
+
     feasible = True
-    while(feasible):
-        if(np.all([abs(i - round(i))<1e-5 for i in x_opt[0:initial_n]])):    #TODO Verify
+    while (feasible):
+        if (np.all([abs(i - round(i)) < 1e-5 for i in x_opt[0:initial_n]])):  # TODO Verify
             x = x_opt[0:initial_n]
             for i in range(len(x)):
                 x[i] = round(x[i])
@@ -28,21 +29,22 @@ def gomory(filename):
             return x
 
         i = choice(x_opt)
-        table,basis_list = addcondition(table,i,basis_list)
+        table, basis_list = addcondition(table, i, basis_list)
 
         n += 1
-        x_opt, flag = dualsimplex(table,n,basis_list)   #TODO Verify
-        if(flag == 1):
+        x_opt, flag = dualsimplex(table, n, basis_list)  # TODO Verify
+        if (flag == 1):
             return None
-        
-        #x_opt = round6(x_opt)   #Verified
-        a,b = table.shape
-        #for i in range(a):
+
+        # x_opt = round6(x_opt)   #Verified
+        a, b = table.shape
+        # for i in range(a):
         #    table[i,:] = round6(table[i,:])
-        
-print(gomory("TC\TC1.txt")) #[1,1]
-print(gomory("TC\TC2.txt")) #[0,2] - #TODO getting [0,1]
-print(gomory("TC\TC3.txt")) #[0,2,0,0]
-print(gomory("TC\TC4.txt")) #[4,4]
-print(gomory("TC\TC5.txt")) #[5,0]
+
+
+print(gomory("TC\TC1.txt"))  # [1,1]
+print(gomory("TC\TC2.txt"))  # [0,2] - #TODO getting [0,1]
+print(gomory("TC\TC3.txt"))  # [0,2,0,0]
+print(gomory("TC\TC4.txt"))  # [4,4]
+print(gomory("TC\TC5.txt"))  # [5,0]
 # print(gomory("TC\TC6.txt")) #[]
